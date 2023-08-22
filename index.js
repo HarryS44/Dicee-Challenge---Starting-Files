553a9f1d6aa5487aabc15d0073165d4da41d7430
Harry Stone <harrystone042006@outlook.com>
2023-08-22 09:29:35 +0100

    Changed up javascript project complete

--- index.js
+++ index.js
@@ -1,50 +1,45 @@
 const randomNumber1 = Math.floor(Math.random() * 6) + 1;
 
 function changeDiceImage() {
-    
+  const imagePaths = {
+    1: "./images/dice1.png",
+    2: "./images/dice2.png",
+    3: "./images/dice3.png",
+    4: "./images/dice4.png",
+    5: "./images/dice5.png",
+    6: "./images/dice6.png",
+  };
 
-    const imagePaths = {
-        1: "./images/dice1.png",
-        2: "./images/dice2.png",
-        3: "./images/dice3.png",
-        4: "./images/dice4.png",
-        5: "./images/dice5.png",
-        6: "./images/dice6.png"
-    };
-
-    const imagePath = imagePaths[randomNumber1];
-
-    document.querySelector("img.img1").setAttribute("src", imagePath);
+  document
+    .querySelector("img.img1")
+    .setAttribute("src", imagePaths[randomNumber1]);
 }
 
-changeDiceImage()
+changeDiceImage();
 
 const randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
 function changeDiceImage2() {
-    
+  const imagePaths = {
+    1: "./images/dice1.png",
+    2: "./images/dice2.png",
+    3: "./images/dice3.png",
+    4: "./images/dice4.png",
+    5: "./images/dice5.png",
+    6: "./images/dice6.png",
+  };
 
-    const imagePaths = {
-        1: "./images/dice1.png",
-        2: "./images/dice2.png",
-        3: "./images/dice3.png",
-        4: "./images/dice4.png",
-        5: "./images/dice5.png",
-        6: "./images/dice6.png"
-    };
-
-    const imagePath = imagePaths[randomNumber2];
-
-    document.querySelector("img.img2").setAttribute("src", imagePath);
+  document
+    .querySelector("img.img2")
+    .setAttribute("src", imagePaths[randomNumber2]);
 }
 
-changeDiceImage2()
+changeDiceImage2();
 
 if (randomNumber1 > randomNumber2) {
-    document.querySelector("h1").innerHTML = "Player 1 wins!";
+  document.querySelector("h1").innerHTML = "Player 1 wins!";
 } else if (randomNumber1 < randomNumber2) {
-    document.querySelector("h1").innerHTML = "Player 2 wins!";
+  document.querySelector("h1").innerHTML = "Player 2 wins!";
 } else if (randomNumber1 === randomNumber2) {
-    document.querySelector("h1").innerHTML = "It's a draw!";
+  document.querySelector("h1").innerHTML = "It's a draw!";
 }
-
